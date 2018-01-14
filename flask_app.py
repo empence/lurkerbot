3e387fdb80f3947f41e415f16507fbd552684be8
Eleanor Pence <eleanor.pence@gmail.com>
2018-01-14 22:38:46 +0000

    added database connection code

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,28 @@
 
-# A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/lurkerbot')
-def hello_world():
-    return render_template("index.html")
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="empence",
+    password="JJaOHH81TMC3",
+    hostname="empence.mysql.pythonanywhere-services.com",
+    databasename="empence$alerts",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@app.route('/lurkerbot', methods=["GET", "POST"])
+def lurkerbot():
+    if request.method == "GET":
+        return render_template("index.html")
+    request.form["phrase"]
+    request.form["subreddit"]
+    request.form["email"]
+    return redirect(url_for("lurkerbot"))
+
 
